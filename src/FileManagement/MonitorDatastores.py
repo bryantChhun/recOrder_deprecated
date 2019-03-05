859c36f70908d88a2865eeefbc32f0a67a28aa4a
Bryant <bchhun@gmail.com>
2019-03-04 16:19:10 -0800

    added simulations
    Former-commit-id: 5d6e4aca005cec0f3ff4cbcbb05f199340cb7112

--- src/FileManagement/MonitorDatastores.py
+++ src/FileManagement/MonitorDatastores.py
@@ -9,10 +9,20 @@
 # python_version  :3.6
 
 import time
-from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
-import threading
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, QThreadPool, QRunnable
 
-from src.DataPipe.PipeFromPy4j import PipeFromPy4j
+
+class ProcessRunnable(QRunnable):
+    def __init__(self, target, args):
+        QRunnable.__init__(self)
+        self.t = target
+        self.args = args
+
+    def run(self):
+        self.t(*self.args)
+
+    def start(self):
+        QThreadPool.globalInstance().start(self)
 
 
 class MonitorDatastores(QObject):
@@ -26,17 +36,25 @@ class MonitorDatastores(QObject):
     '''
     def __init__(self, gate):
         super().__init__()
-        self.gateway = gate
+        self._gateway = gate
         self.ui_ready = True
         self.polstates = set()
+
+    @property
+    def gateway(self):
+        return self._gateway
+
+    @gateway.setter
+    def gateway(self, gateway):
+        self._gateway = gateway
 
     @pyqtSlot(bool)
     def _toggle_switch(self, ready: bool):
         self.ui_ready = ready
 
     def make_connection(self, notify_object):
-        if isinstance(notify_object, PipeFromPy4j):
-            notify_object.poll_newdata.connect(self._toggle_switch)
+        # if isinstance(notify_object, PipeFromPy4j):
+        notify_object.poll_newdata.connect(self._toggle_switch)
         # else:
         #     notify_object.successful_update.connect(self._toggle_switch)
 
@@ -48,33 +66,35 @@ class MonitorDatastores(QObject):
 
             if not self.ui_ready:
                 continue
-            if self.gateway.storeByChannelNameExists("State0") and (0 not in self.polstates):
-                self.newImage.emit((0, self.gateway.retrieveFileByChannelName("State0")))
+            elif not self._gateway:
+                continue
+            elif self._gateway.storeByChannelNameExists("State0") and (0 not in self.polstates):
+                self.newImage.emit((0, self._gateway.retrieveFileByChannelName("State0")))
                 print("Pol State 0 image emitting")
                 self.polstates.add(0)
                 self.ui_ready = False
-            elif self.gateway.storeByChannelNameExists("State1") and (1 not in self.polstates):
-                self.newImage.emit((1, self.gateway.retrieveFileByChannelName("State1")))
+            elif self._gateway.storeByChannelNameExists("State1") and (1 not in self.polstates):
+                self.newImage.emit((1, self._gateway.retrieveFileByChannelName("State1")))
                 print("Pol State 1 image emitting")
                 self.polstates.add(1)
                 self.ui_ready = False
-            elif self.gateway.storeByChannelNameExists("State2") and (2 not in self.polstates):
-                self.newImage.emit((2, self.gateway.retrieveFileByChannelName("State2")))
+            elif self._gateway.storeByChannelNameExists("State2") and (2 not in self.polstates):
+                self.newImage.emit((2, self._gateway.retrieveFileByChannelName("State2")))
                 print("Pol State 2 image emitting")
                 self.polstates.add(2)
                 self.ui_ready = False
-            elif self.gateway.storeByChannelNameExists("State3") and (3 not in self.polstates):
-                self.newImage.emit((3, self.gateway.retrieveFileByChannelName("State3")))
+            elif self._gateway.storeByChannelNameExists("State3") and (3 not in self.polstates):
+                self.newImage.emit((3, self._gateway.retrieveFileByChannelName("State3")))
                 print("Pol State 3 image emitting")
                 self.polstates.add(3)
                 self.ui_ready = False
-            elif self.gateway.storeByChannelNameExists("State4") and (4 not in self.polstates):
-                self.newImage.emit((4, self.gateway.retrieveFileByChannelName("State4")))
+            elif self._gateway.storeByChannelNameExists("State4") and (4 not in self.polstates):
+                self.newImage.emit((4, self._gateway.retrieveFileByChannelName("State4")))
                 print("Pol State 4 image emitting")
                 self.polstates.add(4)
                 self.ui_ready = False
-            elif count >= 150000:
-                #timeout is 2.5 minutes
+            elif count >= 1000:
+                #timeout is 2.5 minutes = 15000
                 print("timeout waiting for more data")
                 break
             elif len(self.polstates) >= 5:
@@ -86,6 +106,6 @@ class MonitorDatastores(QObject):
                     print('waiting')
 
     def run(self):
-        t1 = threading.Thread(target=self._start_monitor)
-        t1.start()
+        self._t1 = ProcessRunnable(target=self._start_monitor, args=())
+        self._t1.start()
 
--- src/FileManagement/RetrieveFiles.py
+++ src/FileManagement/RetrieveFiles.py
@@ -12,7 +12,7 @@ import numpy as np
 import cv2
 import os
 
-from tests.testMetrics import testDataPath
+from tests.testUtils.testMetrics import testDataPath
 
 from src.FileManagement.FileExceptions import InvalidDataTypeError, InvalidChannelNameError, \
     InvalidDataTypeAndChannelError, GatewayNotEstablishedError
@@ -21,18 +21,18 @@ from src.FileManagement.FileExceptions import InvalidDataTypeError, InvalidChann
 Class to retrieve data and return as np.array
     provides support for only Py4J memory mapped files and Test data for now (12/10/2018)
 """
-class RetrieveData():
+
+
+class RetrieveData:
 
     def __init__(self):
         self.types = {'Py4J', 'MemMap', 'File', 'Test'}
         self.states = {'State0', 'State1', 'State2', 'State3', 'State4'}
-        self.sample_types = {'Sample1', 'Sample2', 'BG', 'None'}
+        self.simmap = {'State0':'Iext', 'State1':'I0', 'State2':'I45', 'State3':'I135', 'State4':'I90'}
+        self.sample_types = {'Sample1', 'Sample2', 'BG', 'None', 'Simulation1', 'Simulation2', 'Simulation3'}
         self.gate = None
 
-        # hard code the path to test raw data
         # this value is should be used only when running tests
-        # self.testData = "./testData/rawData/2018_10_02_MouseBrainSlice/"
-        # self.testData = "../tests/testData/rawData/2018_10_02_MouseBrainSlice/"
         self.testData = testDataPath()
 
     def set_gateway(self, gateway):
@@ -47,7 +47,7 @@ class RetrieveData():
         :param channel_name: channel descriptor:
                                 one of "Cy5, DAPI, FITC, Rhodamine" for type="Py4J"
                                 one of "State0, State1, State2, State3, State4" for type="Test"
-        :param type: only support Py4J or Test for now
+        :param type: supports only 'Py4J' or 'Test'
         :param sample_type:
         :return: String filename
         """
@@ -55,29 +55,42 @@ class RetrieveData():
         if sample_type not in self.sample_types:
             raise InvalidDataTypeError("Sample Type must be one of: 'Sample1', 'Sample2', 'BG', 'None'")
 
+        if channel_name not in self.states:
+            raise InvalidChannelNameError("State must be one of:'State0', 'State1', 'State2', 'State3', 'State4'")
+
         if type == 'Py4J':
             if not self.gate:
                 raise GatewayNotEstablishedError("gateway not set for this Py4J pipeline")
             file = self.gate.retrieveFileByChannelName(channel_name)
             if file:
                 return file
+
             else:
                 raise ValueError("no file by channel name exists")
         elif type == 'Test' and sample_type == 'Sample1':
-            if channel_name not in self.states:
-                raise InvalidChannelNameError("State must be one of:'State0', 'State1', 'State2', 'State3', 'State4'")
-            file = self.testData+"SM_2018_1002_1633_1/Pos0/img_000000000_%s - Acquired Image_000.tif" % channel_name
+            file = self.testData+"2018_10_02_MouseBrainSlice/SM_2018_1002_1633_1/Pos0/img_000000000_%s - Acquired Image_000.tif" % channel_name
             return file
+
         elif type == 'Test' and sample_type == 'Sample2':
-            if channel_name not in self.states:
-                raise InvalidChannelNameError("State must be one of:'State0', 'State1', 'State2', 'State3', 'State4'")
-            file = self.testData+"SM_2018_1002_1640_1/Pos0/img_000000000_%s - Acquired Image_000.tif" % channel_name
+            file = self.testData+"2018_10_02_MouseBrainSlice/SM_2018_1002_1640_1/Pos0/img_000000000_%s - Acquired Image_000.tif" % channel_name
             return file
+
+        elif type == 'Test' and sample_type == 'Simulation1':
+            file = self.testData+"simulated/quarter_wave/%s.npy" % self.simmap[channel_name]
+            return file
+
+        elif type == 'Test' and sample_type == 'Simulation2':
+            file = self.testData+"simulated/half_wave/%s.npy" % self.simmap[channel_name]
+            return file
+
+        elif type == 'Test' and sample_type == 'Simulation3':
+            file = self.testData+"simulated/full_wave/%s.npy" % self.simmap[channel_name]
+            return file
+
         elif type == 'Test' and sample_type == 'BG':
-            if channel_name not in self.states:
-                raise InvalidChannelNameError("State must be one of:'State0', 'State1', 'State2', 'State3', 'State4'")
-            file = self.testData+"BG_2018_1002_1625_1/Pos0/img_000000000_%s - Acquired Image_000.tif" % channel_name
+            file = self.testData+"2018_10_02_MouseBrainSlice/BG_2018_1002_1625_1/Pos0/img_000000000_%s - Acquired Image_000.tif" % channel_name
             return file
+
         else:
             raise InvalidDataTypeAndChannelError("Implementation for data type %s and sample type %s not supported"
                                                  % (type, sample_type))
@@ -102,8 +115,11 @@ class RetrieveData():
 
         if type =='Py4J':
             return np.memmap(filename, dtype='uint16', offset=0, mode='r', shape=(512, 512, 1))
-        elif type == 'Test':
+        elif type == 'Test' and sample_type != 'Simulation1':
             img = cv2.imread(filename, -1)
+            return img
+        elif type == 'Test' and sample_type == 'Simulation1':
+            img = np.load(filename, 'r+')
             return img
             # return img.astype(np.float32, copy=False)
 
