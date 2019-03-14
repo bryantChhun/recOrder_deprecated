385d00745e99b873281656a39e98ad7f23034779
bryantchhun <bchhun@gmail.com>
2019-03-14 10:54:01 -0700

    print statement checks to be removed later
    reintroduce meta data store checks for pixel size and depth info
    
    
    Former-commit-id: a962974811f23fdacece376b5a8e368a92f13a2c

--- src/MicroscopeController/Py4jController.py
+++ src/MicroscopeController/Py4jController.py
@@ -47,16 +47,16 @@ def _snap_channel(channel: str, gateway: JavaGateway):
     data_filename = gateway.entry_point.getFile(channel)
     if data_filename is None:
         print("no file with name %s exists" % channel)
-    # store = gateway.entry_point.getStore(channel)
-    # data_pixelshape = (store.x_dim, store.y_dim)
-    # data_pixeldepth = 8*store.bitdepth
-    # if data_pixeldepth == 16:
-    #     depth = np.uint16
-    # elif data_pixeldepth == 8:
-    #     depth = np.uint8
-    # else:
-    #     depth = np.uint16
-    data = deepcopy(np.memmap(filename=data_filename, dtype=np.uint16, offset=0, shape=(512, 512)))
+    store = gateway.entry_point.getStore(channel)
+    data_pixelshape = (store.x_dim, store.y_dim)
+    data_pixeldepth = 8*store.bitdepth
+    if data_pixeldepth == 16:
+        depth = np.uint16
+    elif data_pixeldepth == 8:
+        depth = np.uint8
+    else:
+        depth = np.uint16
+    data = deepcopy(np.memmap(filename=data_filename, dtype=depth, offset=0, shape=data_pixelshape))
     return data
 
 
@@ -94,8 +94,6 @@ def py4j_snap_and_correct(gateway: JavaGateway, background: BackgroundData):
         return False
 
 
-
-
 def py4j_collect_background(gateway: JavaGateway, bg_raw: BackgroundData):
     # add query of State0,1,2,3,4 in config group channel
     # should be accomplished through micromanager
@@ -107,6 +105,7 @@ def py4j_collect_background(gateway: JavaGateway, bg_raw: BackgroundData):
         bg_raw.state4 = _snap_channel('State4', gateway)
         processor = ReconOrder()
         processor.frames = 5
+        print("all states snapped")
 
         #assign intensity states
         processor.state = (0, bg_raw.state0)
@@ -114,21 +113,26 @@ def py4j_collect_background(gateway: JavaGateway, bg_raw: BackgroundData):
         processor.state = (2, bg_raw.state2)
         processor.state = (3, bg_raw.state3)
         processor.state = (4, bg_raw.state4)
+        print("all states assigned to properties")
 
         # construct and assign stokes to bg_raw
         processor.compute_stokes()
+        print("stokes computed")
         bg_raw.s0 = processor.s0
         bg_raw.s1 = processor.s1
         bg_raw.s2 = processor.s2
         bg_raw.s3 = processor.s3
+        print('stokes vectors assigned to background object')
 
         # construct and assign physical to bg_raw
         processor.compute_physical()
+        print("physical computed")
         bg_raw.I_trans = processor.I_trans
         bg_raw.retard = processor.retard
         bg_raw.polarization = processor.polarization
         bg_raw.scattering = processor.scattering
         bg_raw.azimuth = processor.azimuth
+        print("physical assigned to background object")
 
         return bg_raw
 
