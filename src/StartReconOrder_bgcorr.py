a7a417ab3bb068b30c59cf26672b79f0a79ae1a0
Bryant <bchhun@gmail.com>
2019-03-20 17:48:37 -0700

    adjusted for newest napari version
    Former-commit-id: 9e28c0ea70d6f08d71ec7801decc6b64beb01223

--- src/StartReconOrder_bgcorr.py
+++ src/StartReconOrder_bgcorr.py
@@ -14,8 +14,6 @@ This code describes simple execution of Reconstruction and Visualization code.
     and then is displayed in a viewer.
 """
 
-import sys
-from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtWidgets
 
 from src.GUI.NapariWindowOverlay import NapariWindowOverlay
@@ -25,56 +23,53 @@ from src.SignalController.SignalController import SignalController
 from src.Processing.ReconOrder import ReconOrder
 from py4j.java_gateway import JavaGateway
 
-from gui import Window, Viewer
+from napari import ViewerApp
+from napari.util import app_context
 
 if __name__ == '__main__':
-    # starting
-    application = QApplication(sys.argv)
+    with app_context():
 
-    gateway = JavaGateway()
+        gateway = JavaGateway()
 
-    #create Viewer, Windows
-    viewer = Viewer()
-    win = Window(Viewer(), show=False)
-    overlay_window = NapariWindowOverlay(win)
+        #create Viewer, Windows
+        viewer = ViewerApp()
+        overlay_window = NapariWindowOverlay(viewer)
 
-    #initialize file loaders
-    loader = PipeFromFiles(type="Test", sample_type="Sample1")
-    loader_bg = PipeFromFiles(type="Test", sample_type='BG')
+        #initialize file loaders
+        loader = PipeFromFiles(type="Test", sample_type="Sample1")
+        loader_bg = PipeFromFiles(type="Test", sample_type='BG')
 
-    #initialize processors
-    processor = ReconOrder()
-    processor_bg = ReconOrder()
+        #initialize processors
+        processor = ReconOrder()
+        processor_bg = ReconOrder()
 
-    #initialize SignalController
-    signals = SignalController(processor)
+        #initialize SignalController
+        signals = SignalController(processor)
 
-    #Connections: Pipeline to/from Processor
-    processor.frames = 5
-    processor_bg.frames = 5
-    loader.set_processor(processor)
-    loader_bg.set_processor(processor_bg)
+        #Connections: Pipeline to/from Processor
+        processor.frames = 5
+        processor_bg.frames = 5
+        loader.set_processor(processor)
+        loader_bg.set_processor(processor_bg)
 
-    #Connections: Pipeline to/from GUI
-    overlay_window.make_connection(loader)
-    loader.make_connection(overlay_window)
+        #Connections: Pipeline to/from GUI
+        overlay_window.make_connection(loader)
+        loader.make_connection(overlay_window)
 
-    #Connections: SignalController to/from GUI
-    # for gui-initiated pipeline or processing events
-    overlay_window.make_connection(signals)
-    signals.make_connection(overlay_window)
+        #Connections: SignalController to/from GUI
+        # for gui-initiated pipeline or processing events
+        overlay_window.make_connection(signals)
+        signals.make_connection(overlay_window)
 
-    # BGprocess first
-    loader_bg.run_reconstruction(threaded=False)
-    loader.run_reconstruction_BG_correction(loader_bg.get_processor(), threaded=True)
+        # BGprocess first
+        loader_bg.run_reconstruction(threaded=False)
+        loader.run_reconstruction_BG_correction(loader_bg.get_processor(), threaded=True)
 
-    ReconOrderUI = QtWidgets.QDialog()
-    ui = Ui_ReconOrderUI()
-    ui.setupUi(ReconOrderUI)
-    ui.gateway = gateway
-    ReconOrderUI.show()
+        ReconOrderUI = QtWidgets.QDialog()
+        ui = Ui_ReconOrderUI()
+        ui.setupUi(ReconOrderUI)
+        ui.gateway = gateway
+        ReconOrderUI.show()
 
-    #connect so button launches bgprocess
-    ui.assign_pipes(loader, loader_bg)
-
-    sys.exit(application.exec_())
+        #connect so button launches bgprocess
+        ui.assign_pipes(loader, loader_bg)
