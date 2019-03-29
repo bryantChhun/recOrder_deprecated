prevent assignment of new properties to classes hard fixed to receive only PhysicalData Removed try catch Beginning changes to averaging
Former-commit-id: a238c4f2628724689c7677004e9af286bf74057a

src/GUI/NapariWindowOverlay.py
@@ -16,6 +16,7 @@ import numpy as np
 from src.FileManagement.RetrieveFiles import RetrieveData
 from src.DataStructures.PhysicalData import PhysicalData
 from src.Processing.ReconOrder import ReconOrder
+from src.Processing import compute_average, convert_to_vector
 
 
 class NapariWindowOverlay(QWidget):
@@ -50,13 +51,10 @@ class NapariWindowOverlay(QWidget):
 
         #init layers with vector data and subscribe to gui notifications
         self.layer1 = self.viewer.add_vectors(self.pos)
-        self.layer1.averaging_bind_to(self.compute_vector)
-        self.layer1.length_bind_to(self.compute_length)
-
+        # self.layer1._default_avg = compute_average
+        # self.layer1.length_bind_to(self.compute_length)
         self.layer2 = self.viewer.add_image(self.init_data_1, {})
-
         self.layer3 = self.viewer.add_image(self.init_data_1, {})
-
         self.layer4 = self.viewer.add_image(self.init_data_1, {})
 
         self.layers = [self.layer1, self.layer2, self.layer3, self.layer4]
@@ -77,19 +75,22 @@ class NapariWindowOverlay(QWidget):
     @pyqtSlot(object)
     def update_layer_image(self, instance: object):
 
-        if isinstance(instance, ReconOrder) or isinstance(instance, PhysicalData):
+        if isinstance(instance, PhysicalData):
             print("gui received object of type = "+str(type(instance)))
             self.layer1.vectors = instance.azimuth_vector
-            # self.layer2.image = inst_reconOrder.scattering
-            # self.layer2.image = inst_reconOrder.phase
-            self.layer2.image = instance.azimuth_degree
+            self.layer2.image = instance.scattering
             self.layer3.image = instance.retard
             self.layer4.image = instance.I_trans
 
+            self.layer1.name = "vectors"
+            self.layer2.name = 'scattering'
+            self.layer3.name = 'retardance'
+            self.layer4.name = 'transmission'
+
             self.update_complete.emit("Received and updated images")
         else:
-            print("gui received vector or averaging update")
-            self.layer1.vectors = instance
+            print("GUI did not receive Physical Data")
+            # self.layer1.vectors = instance
             # self.layer2.image = 65536*np.random.rand(2048,2048)
 
     def make_connection(self, reconstruction: object):

src/MicroscopeController/Py4jController.py
@@ -31,6 +31,8 @@ def _snap_channel(channel: str, gateway: JavaGateway):
     mmc = gateway.entry_point.getCMMCore()
     mm = gateway.entry_point.getStudio()
 
+    # set channel
+    # for some reason, we need a monitor to be sure it gets set....
     mmc.setChannelGroup('Channel')
     c=0
     while mmc.getCurrentConfig('Channel') != channel:
@@ -46,6 +48,7 @@ def _snap_channel(channel: str, gateway: JavaGateway):
     live_manager = mm.getSnapLiveManager()
     live_manager.snap(True)
 
+    # again we need a monitor to be sure file is written
     ct = 0
     while not gateway.entry_point.fileExists(channel):
         time.sleep(0.001)
@@ -56,6 +59,7 @@ def _snap_channel(channel: str, gateway: JavaGateway):
     print("time to check fileExists = %04d" % ct)
 
     data_filename = gateway.entry_point.getFile(channel)
+
     if data_filename is None:
         print("no file with name %s exists" % channel)
 
@@ -73,7 +77,7 @@ def _snap_channel(channel: str, gateway: JavaGateway):
     return data
 
 
-def py4j_collect_background(gateway: JavaGateway, bg_raw: BackgroundData, averaging: int = 10):
+def py4j_collect_background(gateway: JavaGateway, bg_raw: BackgroundData, averaging: int = 10) -> BackgroundData:
     """
     snaps 10 images of each of state0,1,2,3,4.  Averages the 10 for those states
 
@@ -88,98 +92,74 @@ def py4j_collect_background(gateway: JavaGateway, bg_raw: BackgroundData, averag
     # data_pixelshape = (meta.getxRange(), meta.getyRange())
     data_pixelshape = (2048, 2048)
 
-    try:
-        bg_raw.state0 = np.mean([_snap_channel('State0', gateway).flatten() for i in range(0, averaging)], axis=0)\
-            .reshape(data_pixelshape)
-        bg_raw.state1 = np.mean([_snap_channel('State1', gateway).flatten() for i in range(0, averaging)], axis=0)\
-            .reshape(data_pixelshape)
-        bg_raw.state2 = np.mean([_snap_channel('State2', gateway).flatten() for i in range(0, averaging)], axis=0)\
-            .reshape(data_pixelshape)
-        bg_raw.state3 = np.mean([_snap_channel('State3', gateway).flatten() for i in range(0, averaging)], axis=0)\
-            .reshape(data_pixelshape)
-        bg_raw.state4 = np.mean([_snap_channel('State4', gateway).flatten() for i in range(0, averaging)], axis=0)\
-            .reshape(data_pixelshape)
-        processor = ReconOrder()
-        processor.frames = 5
-        print("all states snapped")
+    bg_raw.state0 = np.mean([_snap_channel('State0', gateway).flatten() for i in range(0, averaging)], axis=0)\
+        .reshape(data_pixelshape)
+    bg_raw.state1 = np.mean([_snap_channel('State1', gateway).flatten() for i in range(0, averaging)], axis=0)\
+        .reshape(data_pixelshape)
+    bg_raw.state2 = np.mean([_snap_channel('State2', gateway).flatten() for i in range(0, averaging)], axis=0)\
+        .reshape(data_pixelshape)
+    bg_raw.state3 = np.mean([_snap_channel('State3', gateway).flatten() for i in range(0, averaging)], axis=0)\
+        .reshape(data_pixelshape)
+    bg_raw.state4 = np.mean([_snap_channel('State4', gateway).flatten() for i in range(0, averaging)], axis=0)\
+        .reshape(data_pixelshape)
+    processor = ReconOrder()
+    processor.frames = 5
+    print("all states snapped")
 
-        #assign intensity states
-        processor.state = (0, bg_raw.state0)
-        processor.state = (1, bg_raw.state1)
-        processor.state = (2, bg_raw.state2)
-        processor.state = (3, bg_raw.state3)
-        processor.state = (4, bg_raw.state4)
-        print("all states assigned to properties")
+    #assign intensity states
+    processor.state = (0, bg_raw.state0)
+    processor.state = (1, bg_raw.state1)
+    processor.state = (2, bg_raw.state2)
+    processor.state = (3, bg_raw.state3)
+    processor.state = (4, bg_raw.state4)
+    print("all states assigned to properties")
 
-        # construct and assign stokes to bg_raw
-        processor.compute_stokes()
-        print("stokes computed")
-        bg_raw.s0 = processor.s0
-        bg_raw.s1 = processor.s1
-        bg_raw.s2 = processor.s2
-        bg_raw.s3 = processor.s3
-        print('stokes vectors assigned to background object')
+    # construct and assign stokes to bg_raw
+    processor.compute_stokes()
+    print("stokes computed")
+    bg_raw.s0 = processor.s0
+    bg_raw.s1 = processor.s1
+    bg_raw.s2 = processor.s2
+    bg_raw.s3 = processor.s3
+    print('stokes vectors assigned to background object')
 
-        # construct and assign physical to bg_raw
-        processor.compute_physical()
-        print("physical computed")
-        bg_raw.I_trans = processor.I_trans
-        bg_raw.retard = processor.retard
-        bg_raw.polarization = processor.polarization
-        bg_raw.scattering = processor.scattering
-        bg_raw.azimuth = processor.azimuth
-        print("physical assigned to background object")
+    # construct and assign physical to bg_raw
+    processor.compute_physical()
+    print("physical computed")
+    bg_raw.I_trans = processor.I_trans
+    bg_raw.retard = processor.retard
+    bg_raw.polarization = processor.polarization
+    bg_raw.scattering = processor.scattering
+    bg_raw.azimuth = processor.azimuth
+    print("physical assigned to background object")
 
-        return bg_raw
+    # write to disk
+    # create a folder with a name
+    # this write should iterate through all BG data stokes? and create a .npy file for each?
 
-        # write to disk
-            # create a folder with a name
-            # this write should iterate through all BG data stokes? and create a .npy file for each?
+    # update UI field to reflect new folder
+    # in reconOrderUI we will have the text field trigger another method that
+    #   will read the .npy files and update ReconOrderUI's self.Background object
 
-        # update UI field to reflect new folder
-        # in reconOrderUI we will have the text field trigger another method that
-        #   will read the .npy files and update ReconOrderUI's self.Background object
-
-    except Exception as ex:
-        print("Exception when collecting background: "+str(ex))
-        return False
+    return bg_raw
 
 
-def py4j_snap_and_correct(gateway: JavaGateway, background: BackgroundData):
+def py4j_snap_and_correct(gateway: JavaGateway, background: BackgroundData) -> PhysicalData:
 
-    bgdat = background
+    temp_int = IntensityData()
+    processor = ReconOrder()
+    processor.frames = 5
 
-    try:
-        temp_int = IntensityData()
-        processor = ReconOrder()
-        processor.frames = 5
+    temp_int.IExt = _snap_channel('State0', gateway)
+    temp_int.I0 = _snap_channel('State1', gateway)
+    temp_int.I45 = _snap_channel('State2', gateway)
+    temp_int.I90 = _snap_channel('State3', gateway)
+    temp_int.I135 = _snap_channel('State4', gateway)
 
-        temp_int.Iext = _snap_channel('State0', gateway)
-        temp_int.I0 = _snap_channel('State1', gateway)
-        temp_int.I45 = _snap_channel('State2', gateway)
-        temp_int.I90 = _snap_channel('State3', gateway)
-        temp_int.I135 = _snap_channel('State4', gateway)
+    temp_stokes = processor.compute_stokes(temp_int)
+    temp_physical = processor.correct_background(temp_stokes, background)
 
-        processor.IntensityData = temp_int
-    except Exception as ex:
-        print("Exception when snapping image: " + str(ex))
-        return False
-
-    try:
-        temp_physical = PhysicalData()
-        processor.compute_stokes()
-        processor.compute_physical()
-        processor.correct_background(bgdat)
-        temp_physical.I_trans = processor.I_trans
-        temp_physical.retard = processor.retard
-        temp_physical.polarization = processor.polarization
-        temp_physical.scattering = processor.scattering
-        temp_physical.azimuth = processor.azimuth
-
-        return temp_physical
-    except Exception as ex:
-        print("Exception when processing background: " +str(ex))
-        return False
+    return temp_physical
 
 def py4j_calibrate_lc(gateway: JavaGateway):
     #create processing module

src/Processing/AzimuthToVector.py
@@ -11,14 +11,16 @@
 import numpy as np
 from typing import Union
 
-import cv2
+from scipy import signal
+
+from src.DataStructures import StokesData
 
 
 def convert_to_vector(azimuth: np.array,
                       retardance: np.array,
                       stride_x: int = 1,
                       stride_y: int = 1,
-                      length: Union[int, float] = 10) -> np.array:
+                      length: Union[int, float] = 1) -> np.array:
     """
     This function converts an "azimuth" (radian) array into a vector array
     Method:
@@ -42,44 +44,40 @@ def convert_to_vector(azimuth: np.array,
     #create empty vector of necessary shape
     # every pixel has 2 coordinates,
     pos = np.zeros((xdim, ydim, 2), dtype=np.float32)
-    pos[:, :, 0] = (stride_x/2) * length * retardance * np.cos(azimuth)
-    pos[:, :, 1] = (stride_y/2) * length * retardance * np.sin(azimuth)
+    pos[:, :, 0] = (stride_x//2) * length * retardance * np.cos(azimuth)
+    pos[:, :, 1] = (stride_y//2) * length * retardance * np.sin(azimuth)
 
     return pos
 
 
-def compute_average(s1,
-                    s2,
-                    s3,
-                    kernel: tuple = (1,1),
-                    length: Union[int, float] = 10,
+def compute_average(stk_img : StokesData,
+                    kernel : int = 1,
+                    length: Union[int, float] = 1,
                     flipPol=False) -> np.array:
-    """
-    This function computes the average azimuth_vector from the constituent stokes vectors
 
-    *** timed at roughly 43ms compute ***
-
-    :param s1: stokes image of type np.ndarray
-    :param s2: stokes image of type np.ndarray
-    :param kernel: dims over which averaging occurs.
-    :param length:
-    :param flipPol:
-    :return:
-    """
-    x = kernel[0]
-    y = kernel[1]
+    x, y = kernel, kernel
     x_offset = int((x-1)/2)
     y_offset = int((y-1)/2)
 
-    s1_avg = cv2.blur(s1, (x,y))[x_offset:-x_offset-1:x, y_offset:-y_offset-1:y]
-    s2_avg = cv2.blur(s2, (x,y))[x_offset:-x_offset-1:x, y_offset:-y_offset-1:y]
-    s3_avg = cv2.blur(s3, (x,y))[x_offset:-x_offset-1:x, y_offset:-y_offset-1:y]
+    # if (x, y) == (1, 1):
+    #     self.vectors = self._original_data
+    #     return
+    #     # return "calling original data"
+
+    kernel = np.ones(shape=(x, y)) / (x * y)
+
+    s1_avg = signal.convolve2d(stk_img.s1[:, :, 0], kernel, mode='same', boundary='wrap')
+    s2_avg = signal.convolve2d(stk_img.s2[:, :, 1], kernel, mode='same', boundary='wrap')
+    s3_avg = signal.convolve2d(stk_img.s3[:, :, 1], kernel, mode='same', boundary='wrap')
 
     if flipPol == 'rcp':
         azimuth_avg = (0.5 * np.arctan2(-s1_avg, s2_avg) + 0.5 * np.pi)  # make azimuth fall in [0,pi]
     else:
         azimuth_avg = (0.5 * np.arctan2(s1_avg, s2_avg) + 0.5 * np.pi)  # make azimuth fall in [0,pi]
     retard_avg = np.arctan2(np.sqrt(s1_avg ** 2 + s2_avg ** 2), s3_avg) / (2 * np.pi)
+
+    # self.vectors = output_mat[x_offset:range_x - x_offset:x, y_offset:range_y - y_offset:y]
+    # self.length = self._length
 
     vectors = convert_to_vector(azimuth_avg - (0.5 * np.pi), retardance=retard_avg, stride_x=x, stride_y=y, length=length)
 

src/Processing/ReconOrder.py
@@ -15,7 +15,7 @@ from ..DataStructures import PhysicalData
 from ..DataStructures import StokesData
 from ..DataStructures import BackgroundData
 
-from src.Processing.AzimuthToVector import compute_average
+from src.Processing.AzimuthToVector import convert_to_vector
 
 
 '''
@@ -44,9 +44,6 @@ class ReconOrder(object):
     def __init__(self):
         super(ReconOrder, self).__init__()
 
-        # self._intensity = None
-        # self._stokes = None
-        # self._physical = None
         self._frames = None
 
         self.height = None
@@ -62,49 +59,6 @@ class ReconOrder(object):
 
     # ==== properties ====
 
-    # @property
-    # def IntensityData(self):
-    #     return self._intensity
-    #
-    # @IntensityData.setter
-    # def IntensityData(self, int_obj: IntensityData):
-    #     """
-    #     is assigned by creating an IntensityData object
-    #         Intensity data images are assigned to that object
-    #         Then this parameter is assigned to that object
-    #     :param int_obj: Object of type IntensityData
-    #     :return:
-    #     """
-    #     self._intensity = int_obj
-    #
-    # @property
-    # def StokesData(self):
-    #     return self._stokes
-    #
-    # @StokesData.setter
-    # def StokesData(self, stk_obj: StokesData):
-    #     """
-    #     is assigned through compute_stokes or through this property setter
-    #
-    #     :param stk_obj: Object of type StokesData
-    #     :return:
-    #     """
-    #     self._stokes = stk_obj
-    #
-    # @property
-    # def PhysicalData(self):
-    #     return self._physical
-    #
-    # @PhysicalData.setter
-    # def PhysicalData(self, phy_obj: PhysicalData):
-    #     """
-    #     is assigned through compute_physical or through this property setter
-    #
-    #     :param phy_obj: Object of type PhysicalData
-    #     :return:
-    #     """
-    #     self._physical = phy_obj
-
     @property
     def frames(self) -> int:
         return self._frames
@@ -117,7 +71,7 @@ class ReconOrder(object):
         :return:
         """
         if num_frames != 4 and num_frames != 5:
-            raise InvalidFrameNumberDeclarationError("support only 4 or 5 frame reconstructions")
+            raise NotImplementedError("support only 4 or 5 frame reconstructions")
         else:
             self._frames = num_frames
 
@@ -139,28 +93,27 @@ class ReconOrder(object):
 
     # ==== compute functions ====
 
-    def correct_background(self, stk_obj: StokesData, phy_obj: PhysicalData, background: BackgroundData):
+    def correct_background(self, stk_obj: StokesData, background: BackgroundData) -> PhysicalData:
         """
         Uses computed result from background images to calculate the correction
         :param stk_obj: StokesData
-        :param phy_obj: PhysicalData
         :param background: ReconOrder object that is constructed from background images
         :return:
         """
 
-        # assign class data if none is passed
-        # if stk_obj is None and self.StokesData is not None:
-        #     stk_obj = self.StokesData
-        # if phy_obj is None and self.PhysicalData is not None:
-        #     phy_obj = self.PhysicalData
-
         if isinstance(background, BackgroundData):
-            phy_obj.I_trans = phy_obj.I_trans / background.I_trans
-            phy_obj.polarization = phy_obj.polarization / background.polarization
             stk_obj.A = stk_obj.A - background.A
             stk_obj.B = stk_obj.B - background.B
+            stk_obj.s0 = stk_obj.s0 / background.s0
+            stk_obj.s3 = stk_obj.s3 / background.s3
+
+            new_phys = self.compute_physical(stk_obj)
+
+            new_phys.polarization = new_phys.polarization / background.polarization
+
+            return new_phys
         else:
-            raise InvalidBackgroundObject("background parameter must be a ReconOrder object or None (for local Gauss)")
+            raise ModuleNotFoundError("background parameter must be a ReconOrder object")
 
     def compute_inst_matrix(self):
         chi = self._swing_rad
@@ -176,7 +129,7 @@ class ReconOrder(object):
                                  [1, -np.sin(chi), 0, -np.cos(chi)],
                                  [1, 0, -np.sin(chi), -np.cos(chi)]])
         else:
-            raise InvalidFrameNumberDeclarationError('Frames not set to 4 or 5:  Required for calculation of instrument matrix')
+            raise NotImplementedError('Frames not set to 4 or 5:  Required for calculation of instrument matrix')
 
         self.inst_mat_inv = np.linalg.pinv(inst_mat)
 
@@ -186,31 +139,16 @@ class ReconOrder(object):
 
         :return: populated StokesData
         """
-        # if int_obj is None and self.IntensityData is not None:
-        #     int_obj = self.IntensityData
-        # elif int_obj is None:
-        #     raise ModuleNotFoundError("no Intensity data passed or assigned to this reconstruction")
 
         output_stokes = StokesData()
-        chi = self._swing_rad
 
         # define our instrument matrix based on self.frames
         if self._frames == 4:
             img_raw = np.stack((int_obj.IExt, int_obj.I45, int_obj.I90, int_obj.I135))  # order the channel following stokes calculus convention
             n_chann = np.shape(img_raw)[0]
-            # inst_mat = np.array([[1, 0, 0, -1],
-            #                      [1, 0, np.sin(chi), -np.cos(chi)],
-            #                      [1, -np.sin(chi), 0, -np.cos(chi)],
-            #                      [1, 0, -np.sin(chi), -np.cos(chi)]])
         elif self._frames == 5:
-            # int_obj.I0 = self.state[4]
             img_raw = np.stack((int_obj.IExt, int_obj.I0, int_obj.I45, int_obj.I90, int_obj.I135))  # order the channel following stokes calculus convention
             n_chann = np.shape(img_raw)[0]
-            # inst_mat = np.array([[1, 0, 0, -1],
-            #                      [1, np.sin(chi), 0, -np.cos(chi)],
-            #                      [1, 0, np.sin(chi), -np.cos(chi)],
-            #                      [1, -np.sin(chi), 0, -np.cos(chi)],
-            #                      [1, 0, -np.sin(chi), -np.cos(chi)]])
         else:
             raise ValueError("frames must be 4 or 5 to perform stokes calculation")
 
@@ -218,18 +156,15 @@ class ReconOrder(object):
         self.width = int_obj.IExt.shape[1]
 
         # calculate stokes
-        # inst_mat_inv = np.linalg.pinv(inst_mat)
         img_raw_flat = np.reshape(img_raw,(n_chann, self.height*self.width))
         img_stokes_flat = np.dot(self.inst_mat_inv, img_raw_flat)
         img_stokes = np.reshape(img_stokes_flat, (img_stokes_flat.shape[0], self.height, self.width))
 
         [output_stokes.s0, output_stokes.s1, output_stokes.s2, output_stokes.s3] = [img_stokes[i, :, :] for i in range(0, img_stokes.shape[0])]
 
-        # if there's no stokes data, do we assign it here?
-
         return output_stokes
 
-    def compute_physical(self, stk_obj: StokesData, flip_pol='rcp', avg_kernel=(1, 1)) -> PhysicalData:
+    def compute_physical(self, stk_obj: StokesData, flip_pol='rcp') -> PhysicalData:
         """
         computes physical results from the stokes vectors
             transmittance
@@ -240,19 +175,13 @@ class ReconOrder(object):
             azimuth_vector
         :param stk_obj: object of type StokesData
         :param flip_pol: whether azimuth is flipped based on rcp/lcp analyzer
-        :param avg_kernel: kernel over which to average azimuth
         :return: PhysicalData object
         """
-        # if stk_obj is None and self.StokesData is not None:
-        #     stk_obj = self.StokesData
-        # elif stk_obj is None:
-        #     raise ModuleNotFoundError("no Stokes data passed or assigned to this reconstruction")
 
         output_physical = PhysicalData()
 
-        # compute s1 and s2 from background corrected A and B
-        s1 = stk_obj.A * stk_obj.s3
-        s2 = -stk_obj.B * stk_obj.s3
+        s1 = stk_obj.s1
+        s2 = stk_obj.s2
 
         output_physical.I_trans = stk_obj.s0
         output_physical.retard = np.arctan2(np.sqrt(s1 ** 2 + s2 ** 2), stk_obj.s3)
@@ -269,20 +198,16 @@ class ReconOrder(object):
         #make the arrays displayable by scaling to more meaningful values
         output_physical.retard = output_physical.retard / (2 * np.pi) * self.wavelength  # convert the unit to [nm]
         output_physical.azimuth_degree = output_physical.azimuth / (np.pi) * 180
-        # output_physical.azimuth_degree = output_physical.azimuth/np.pi*180
-        # self.azimuth_vector = convert_to_vector(self.azimuth - (0.5*np.pi))
-        # _, _, output_physical.azimuth_vector = compute_average(s1, s2, stk_obj.s3, kernel=avg_kernel, length=1, flipPol=flip_pol)
+        output_physical.azimuth_vector = convert_to_vector(output_physical.azimuth - (0.5*np.pi),
+                                                           output_physical.retard)
 
         return output_physical
 
     # ==== data scaling ====
 
     def rescale_bitdepth(self, phy_obj: PhysicalData):
-        # if phy_obj is None and self.PhysicalData is not None:
-        #     phy_obj = self.PhysicalData
 
         phy_obj.retard = phy_obj.retard / (2 * np.pi) * self.wavelength  # convert the unit to [nm]
-        # phy_obj.azimuth_degree = phy_obj.azimuth/np.pi*180
 
         phy_obj.I_trans = self.imBitConvert(phy_obj.I_trans * 10 ** 3, bit=16, norm=True)  # AU, set norm to False for tiling images
         phy_obj.retard = self.imBitConvert(phy_obj.retard * 10 ** 3, bit=16)  # scale to pm
@@ -303,24 +228,3 @@ class ReconOrder(object):
         else:
             im = im.astype(np.uint16, copy=False)  # convert to 16 bit
         return im
-
-
-class InsufficientDataError(Exception):
-
-    def __init__(self, message):
-        Exception.__init__(self, message)
-        self.message = message
-
-
-class InvalidFrameNumberDeclarationError(Exception):
-
-    def __init__(self, message):
-        Exception.__init__(self, message)
-        self.message = message
-
-
-class InvalidBackgroundObject(Exception):
-
-    def __init__(self, message):
-        Exception.__init__(self, message)
-        self.message = message